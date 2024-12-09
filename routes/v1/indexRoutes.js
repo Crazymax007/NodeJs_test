50f4b2bb245afcfa7e0a230baf1de7537cc0cc43
Crazymax <140380275+Crazymax007@users.noreply.github.com>
2024-12-09 23:15:18 +0700

    as

--- routes/v1/indexRoutes.js
+++ routes/v1/indexRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { pageTitle: 'HiddenGem Lab Backend (Test2)',featuredContent:'adfdfsdfsd'});
+  res.render('index', { pageTitle: 'Crazymax Back-End (Test)',featuredContent:'adfdfsdfsd'});
 });
 
 module.exports = router;
